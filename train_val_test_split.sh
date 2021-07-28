0041d43b955b2dc88b58e2cbd24191b6876ccdba
Donato Meoli <donato.meoli.95@gmail.com>
2021-07-29 01:55:16 +0200

    updated data splitting .sh file

--- train_val_test_split.sh
+++ train_val_test_split.sh
@@ -1,10 +1,10 @@
 PROBLEM_TYPE="${1:-uniform-random-3-sat}"
 
-rm -rf ./data/"$PROBLEM_TYPE"/train
-rm -rf ./data/"$PROBLEM_TYPE"/val
-rm -rf ./data/"$PROBLEM_TYPE"/test
+rm -rf data/"$PROBLEM_TYPE"/train
+rm -rf data/"$PROBLEM_TYPE"/val
+rm -rf data/"$PROBLEM_TYPE"/test
 
-splitfolders --output ./data/"$PROBLEM_TYPE" \
+splitfolders --output data/"$PROBLEM_TYPE" \
   --ratio 0.8 0.1 0.1 \
   --seed 123456 \
-  -- ./data/"$PROBLEM_TYPE"
+  -- data/"$PROBLEM_TYPE"
