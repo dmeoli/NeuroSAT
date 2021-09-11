f458901da2788f36d2d01b09170a0d7601eb7f93
Donato Meoli <donato.meoli.95@gmail.com>
2021-09-11 17:29:44 +0200

    updated sh file

--- train_val_test_split.sh
+++ train_val_test_split.sh
@@ -8,3 +8,13 @@ splitfolders --output data/"$PROBLEM_TYPE" \
   --ratio 0.8 0.1 0.1 \
   --seed 123456 \
   -- data/"$PROBLEM_TYPE"
+
+if [ "$PROBLEM_TYPE" == "uniform-random-3-sat" ]; then
+  rm -rf data/"$PROBLEM_TYPE"/train/'uf250-1065'
+  rm -rf data/"$PROBLEM_TYPE"/val/'uf250-1065'
+  rm -rf data/"$PROBLEM_TYPE"/test/'uf250-1065'
+
+  rm -rf data/"$PROBLEM_TYPE"/train/'uuf250-1065'
+  rm -rf data/"$PROBLEM_TYPE"/val/'uuf250-1065'
+  rm -rf data/"$PROBLEM_TYPE"/test/'uuf250-1065'
+fi
