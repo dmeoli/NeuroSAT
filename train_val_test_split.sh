bf5e10123cc69bea06bdecbaeff3604cccc381ed
dmeoli <donato.meoli.95@gmail.com>
2021-12-07 19:49:19 +0100

    updated requirements

--- train_val_test_split.sh
+++ train_val_test_split.sh
@@ -18,7 +18,7 @@ if [ "$PROBLEM_TYPE" = "uniform-random-3-sat" ]; then
   rm -r data/"$PROBLEM_TYPE"/train/uuf250-1065
   rm -r data/"$PROBLEM_TYPE"/val/uuf250-1065
   rm -r data/"$PROBLEM_TYPE"/test/uuf250-1065
-  
+
 elif [ "$PROBLEM_TYPE" = "graph-coloring" ]; then
 
   rm -r data/"$PROBLEM_TYPE"/train/flat30-60
@@ -28,7 +28,7 @@ elif [ "$PROBLEM_TYPE" = "graph-coloring" ]; then
   rm -r data/"$PROBLEM_TYPE"/train/flat150-360
   rm -r data/"$PROBLEM_TYPE"/train/flat175-417
   rm -r data/"$PROBLEM_TYPE"/train/flat200-479
-  
+
   rm -r data/"$PROBLEM_TYPE"/val/flat30-60
   rm -r data/"$PROBLEM_TYPE"/val/flat75-180
   rm -r data/"$PROBLEM_TYPE"/val/flat100-239
@@ -36,7 +36,7 @@ elif [ "$PROBLEM_TYPE" = "graph-coloring" ]; then
   rm -r data/"$PROBLEM_TYPE"/val/flat150-360
   rm -r data/"$PROBLEM_TYPE"/val/flat175-417
   rm -r data/"$PROBLEM_TYPE"/val/flat200-479
-  
+
   rm -r data/"$PROBLEM_TYPE"/test/flat30-60
   rm -r data/"$PROBLEM_TYPE"/test/flat75-180
   rm -r data/"$PROBLEM_TYPE"/test/flat100-239
@@ -44,5 +44,5 @@ elif [ "$PROBLEM_TYPE" = "graph-coloring" ]; then
   rm -r data/"$PROBLEM_TYPE"/test/flat150-360
   rm -r data/"$PROBLEM_TYPE"/test/flat175-417
   rm -r data/"$PROBLEM_TYPE"/test/flat200-479
-  
+
 fi
