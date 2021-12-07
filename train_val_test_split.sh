d14eb7cb61289f265f5f8130a8d9fd4e6e7f8e8a
dmeoli <donato.meoli.95@gmail.com>
2021-12-07 11:50:38 +0100

    updated train_val_test_split.sh

--- train_val_test_split.sh
+++ train_val_test_split.sh
@@ -9,12 +9,48 @@ splitfolders --output data/"$PROBLEM_TYPE" \
   --seed 123456 \
   -- data/"$PROBLEM_TYPE"
 
-if [ "$PROBLEM_TYPE" == "uniform-random-3-sat" ]; then
-  rm data/"$PROBLEM_TYPE"/train/'uf250-1065'
-  rm data/"$PROBLEM_TYPE"/val/'uf250-1065'
-  rm data/"$PROBLEM_TYPE"/test/'uf250-1065'
+if [ "$PROBLEM_TYPE" = "uniform-random-3-sat" ]; then
 
-  rm data/"$PROBLEM_TYPE"/train/'uuf250-1065'
-  rm data/"$PROBLEM_TYPE"/val/'uuf250-1065'
-  rm data/"$PROBLEM_TYPE"/test/'uuf250-1065'
+  rm -r data/"$PROBLEM_TYPE"/train/uf250-1065
+  rm -r data/"$PROBLEM_TYPE"/val/uf250-1065
+  rm -r data/"$PROBLEM_TYPE"/test/uf250-1065
+
+  rm -r data/"$PROBLEM_TYPE"/train/uuf250-1065
+  rm -r data/"$PROBLEM_TYPE"/val/uuf250-1065
+  rm -r data/"$PROBLEM_TYPE"/test/uuf250-1065
+  
+elif [ "$PROBLEM_TYPE" = "graph-coloring" ]; then
+
+  rm -r data/"$PROBLEM_TYPE"/train/flat30-60
+  rm -r data/"$PROBLEM_TYPE"/train/flat75-180
+  rm -r data/"$PROBLEM_TYPE"/train/flat100-239
+  rm -r data/"$PROBLEM_TYPE"/train/flat125-301
+  rm -r data/"$PROBLEM_TYPE"/train/flat150-360
+  rm -r data/"$PROBLEM_TYPE"/train/flat175-417
+  rm -r data/"$PROBLEM_TYPE"/train/flat200-479
+  
+  rm -r data/"$PROBLEM_TYPE"/val/flat30-60
+  rm -r data/"$PROBLEM_TYPE"/val/flat75-180
+  rm -r data/"$PROBLEM_TYPE"/val/flat100-239
+  rm -r data/"$PROBLEM_TYPE"/val/flat125-301
+  rm -r data/"$PROBLEM_TYPE"/val/flat150-360
+  rm -r data/"$PROBLEM_TYPE"/val/flat175-417
+  rm -r data/"$PROBLEM_TYPE"/val/flat200-479
+  
+  rm -r data/"$PROBLEM_TYPE"/test/flat30-60
+  rm -r data/"$PROBLEM_TYPE"/test/flat75-180
+  rm -r data/"$PROBLEM_TYPE"/test/flat100-239
+  rm -r data/"$PROBLEM_TYPE"/test/flat125-301
+  rm -r data/"$PROBLEM_TYPE"/test/flat150-360
+  rm -r data/"$PROBLEM_TYPE"/test/flat175-417
+  rm -r data/"$PROBLEM_TYPE"/test/flat200-479
+  
+  cp -r data/"$PROBLEM_TYPE"/flat30-60 data/"$PROBLEM_TYPE"/test
+  cp -r data/"$PROBLEM_TYPE"/flat75-180 data/"$PROBLEM_TYPE"/test
+  cp -r data/"$PROBLEM_TYPE"/flat100-239 data/"$PROBLEM_TYPE"/test
+  cp -r data/"$PROBLEM_TYPE"/flat125-301 data/"$PROBLEM_TYPE"/test
+  cp -r data/"$PROBLEM_TYPE"/flat150-360 data/"$PROBLEM_TYPE"/test
+  cp -r data/"$PROBLEM_TYPE"/flat175-417 data/"$PROBLEM_TYPE"/test
+  cp -r data/"$PROBLEM_TYPE"/flat200-479 data/"$PROBLEM_TYPE"/test
+  
 fi
